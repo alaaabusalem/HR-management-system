468e43229c9b2be190695466484307016a6d690a
alaaabusalem <alaa.abusalem94@hotmail.com>
2023-04-09 11:43:28 +0000

    updating

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict'
 function Employee(fullName, emId, department, level, imgUrl, salary){
    this.fullName=fullName;
    this.emId=emId;
@@ -7,49 +8,49 @@ function Employee(fullName, emId, department, level, imgUrl, salary){
     this.salary=salary
 }
 
-Employee.prototype.calculateNetSalary=function(level){
-    let salary=0;
-if (level=="Senior"){
-     salary=Math.floor(Math.random() * (2000 - 1500 + 1) + 1500)  
+Employee.prototype.calculateNetSalary=function(){
+    let sala=0;
+if (this.level=="Senior"){
+     sala=Math.floor(Math.random() * (2000 - 1500 + 1) + 1500)  
 }
-if (level=="Mid-Senior"){
-    salary=Math.floor(Math.random() * (1500 - 1000 + 1) + 1000)  
+if (this.level=="Mid-Senior"){
+    sala=Math.floor(Math.random() * (1500 - 1000 + 1) + 1000)  
 }
-if (level=="Junior"){
-    salary=Math.floor(Math.random() * (1000 - 500 + 1) + 500)  
+if (this.level=="Junior"){
+    sala=Math.floor(Math.random() * (1000 - 500 + 1) + 500)  
 }
-return salary-(salary/7.5)
+return sala-(sala*(7.5/100))
 }
-Employee.prototype.renderEachEmplyee=function(fullName,salary){
-    document.write(`</br> </br>${fullName} &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  the salary ------>>>> ${salary}</br>`)
+Employee.prototype.renderEachEmplyee=function(){
+    document.write(`</br> </br> ${this.fullName} &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  the salary ------>>>> ${this.salary}</br>`)
 }
 const ghaziSamer=new Employee('Ghazi Samer',1000,'Administration','Senior','em1.jpg',0);
-ghaziSamer.salary=ghaziSamer.calculateNetSalary(ghaziSamer.level);
-ghaziSamer.renderEachEmplyee(ghaziSamer.fullName,ghaziSamer.salary);
+ghaziSamer.salary=ghaziSamer.calculateNetSalary();
+ghaziSamer.renderEachEmplyee();
 
 
 let lanaAli=new Employee('Lana Ali',1001,'Finance','Senior','em3.jpg',0);
-lanaAli.salary=lanaAli.calculateNetSalary(lanaAli.level);
-lanaAli.renderEachEmplyee(lanaAli.fullName,lanaAli.salary);
+lanaAli.salary=lanaAli.calculateNetSalary();
+lanaAli.renderEachEmplyee();
 
 let tamaraAyoub=new Employee('Tamara Ayoub',1002,'Marketing','Senior','em6.avif',0);
-tamaraAyoub.salary=tamaraAyoub.calculateNetSalary(tamaraAyoub.level);
-tamaraAyoub.renderEachEmplyee(tamaraAyoub.fullName,tamaraAyoub.salary);
+tamaraAyoub.salary=tamaraAyoub.calculateNetSalary();
+tamaraAyoub.renderEachEmplyee();
 
 let safiWalid=new Employee('Safi Walid',1003,'Administration','Mid-Senior','em4.jpg',0);
-safiWalid.salary=safiWalid.calculateNetSalary(safiWalid.level);
-safiWalid.renderEachEmplyee(safiWalid.fullName,safiWalid.salary);
+safiWalid.salary=safiWalid.calculateNetSalary();
+safiWalid.renderEachEmplyee();
 
 let omarZaid=new Employee('Omar Zaid',1004,'Development','Senior','em5.png',0);
-omarZaid.salary=omarZaid.calculateNetSalary(omarZaid.level);
-omarZaid.renderEachEmplyee(omarZaid.fullName,omarZaid.salary);
+omarZaid.salary=omarZaid.calculateNetSalary();
+omarZaid.renderEachEmplyee();
 
 let ranaSaleh=new Employee('Rana Saleh',1005,'Development','Junior','em3.jpg',0);
-ranaSaleh.salary=ranaSaleh.calculateNetSalary(ranaSaleh.level);
-ranaSaleh.renderEachEmplyee(ranaSaleh.fullName,ranaSaleh.salary);
+ranaSaleh.salary=ranaSaleh.calculateNetSalary();
+ranaSaleh.renderEachEmplyee();
 
 let hadiAhmad=new Employee('Hadi Ahmad',1006,'Finance','Mid-Senior','em1.jpg',0);
-hadiAhmad.salary=hadiAhmad.calculateNetSalary(hadiAhmad.level);
-hadiAhmad.renderEachEmplyee(hadiAhmad.fullName,hadiAhmad.salary);
+hadiAhmad.salary=hadiAhmad.calculateNetSalary();
+hadiAhmad.renderEachEmplyee();
 
 
